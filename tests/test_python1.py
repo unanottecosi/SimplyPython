0f7d66fe9b90ee1c455d6e0e71477063b71ac676
unanottecosi <30680914+unanottecosi@users.noreply.github.com>
2024-01-16 15:28:22 -0800

    Update test_python1.py

--- tests/test_python1.py
+++ tests/test_python1.py
@@ -2,6 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
+from time import sleep
+'''
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+'''
 
 def test_simple1():
   chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
@@ -11,6 +17,9 @@ def test_simple1():
   chrome_options = webdriver.ChromeOptions()    
   # Add your options as needed    
   options = [
+      # Define window size here
+      "--window-size=1200,1200",
+      "--ignore-certificate-errors"
       #"--headless"
       #"--disable-gpu",
       #"--window-size=1920,1200",
@@ -28,5 +37,8 @@ def test_simple1():
   driver = webdriver.Chrome(options = chrome_options)
   
   driver.get('http://github.com')
+  sleep(5)
   print("\nDriver Title: " + driver.title)
+  sleep(2)
+  driver.quit()
   
