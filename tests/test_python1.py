bdf987e82eebb9534ced4a60893805a8fca0967f
unanottecosi <30680914+unanottecosi@users.noreply.github.com>
2024-01-16 14:57:20 -0800

    Create test_python1.py

---
+++ tests/test_python1.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
+def test_simple1():
+  chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                        # and if it doesn't exist, download it automatically,
+                                        # then add chromedriver to path
+  
+  chrome_options = webdriver.ChromeOptions()    
+  # Add your options as needed    
+  options = [
+      #"--headless"
+      #"--disable-gpu",
+      #"--window-size=1920,1200",
+      #"--ignore-certificate-errors",
+      #"--disable-extensions",
+      #"--no-sandbox",
+      #"--disable-dev-shm-usage",
+      #'--remote-debugging-port=9222'
+  ]
+  
+  for option in options:
+      chrome_options.add_argument(option)
+  
+      
+  driver = webdriver.Chrome(options = chrome_options)
+  
+  driver.get('http://github.com')
+  print("\nDriver Title: " + driver.title)
+  
